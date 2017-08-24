3d01af748c0c9f03f6b86ad3cea413111fd630b2
meynm <meynmd@users.noreply.gibhub.com>
2017-08-24 14:18:07 -0700

    heuristic

--- maxrepeat.py
+++ maxrepeat.py
@@ -1,30 +1,41 @@
+from collections import defaultdict
 """
 maxrepeat(s) finds all occurrences of max length repeated substring in s
 """
-
 def maxrepeat(s):
-    current, best, occurrences = [], [], []
+    current, best = [], []
+    occurrences = defaultdict(set)
 
     # split at each possible point
     for offset in range(1, len(s) - 1):
         start = 0
 
         # compare elements offset by this distance
-        for i in range(len(s) - offset):
-
+        last = len(s) - offset
+        for i in range(last):
             if s[i] == s[offset + i]:
                 # building the substring
                 current.append(s[i])
-            else:
-                # substring has ended
-                if len(current) > len(best):
-                    best = current
-                    occurrences = [(start, i), (start + offset, i + offset)]
-                elif len(current) == len(best):
-                    occurrences += [(start, i), (start + offset, i + offset)]
-                start = i
-                current = []
+                if i < last - 1:
+                    continue
 
-    return best, occurrences
+            # substring has ended
+            if i == last - 1:
+                i += 1
+            if len(current) > len(best):
+                best = current
+                occurrences = defaultdict(
+                    set, {tuple(best) : {
+                        (start, i),
+                        (start + offset, i + offset)
+                    }})
+            elif len(current) == len(best) and len(best) > 0:
+                occurrences[tuple(current)].add((start, i))
+                occurrences[tuple(current)].add((start + offset, i + offset))
+
+            start = i + 1
+            current = []
+
+    return max(occurrences.items(), key=lambda x: len(x[0]) * len(x[1]))
 
 
--- smallgram.py
+++ smallgram.py
@@ -1,10 +1,27 @@
 #!/bin/python
 
 from maxrepeat import *
+from symbols import *
 
 D_MAX = 3
 
-# def compressGrammar(grammar, depth)
+def compressGrammar(grammar, depth):
+    grammar = grammarToList(grammar)
+    # while True:
+    #
+    # return
 
-testStr = list("febcdcdebcdfgdcd")
-print maxrepeat(testStr)
+def grammarToList(grammarDictionary):
+    grammarList = []
+    count = 0
+    for key, val in grammarDictionary.items():
+        count += 1
+        grammarList.append(key)
+        grammarList += val
+
+    return grammarList
+
+
+teststr = list("fabcdeabcgabcfeab")
+
+print maxrepeat(teststr)
